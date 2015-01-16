4e48e1f1dae1888f5a48c28edcaea0d8d81dce23
TWilkin <tom@xzi-xzone.com>
2015-01-16 15:45:54 +0000

    Moved LDAP code from DomeoDashboard to Domeo project. Added default profile to LDAP generated users.

--- DomeoDashboard/grails-app/conf/spring/resources.groovy
+++ DomeoDashboard/grails-app/conf/spring/resources.groovy
@@ -1,14 +1,4 @@
-import org.mindinformatics.grails.domeo.dashboard.security.ldap.LDAPAuthenticationApplicationListener;
-import org.mindinformatics.grails.domeo.dashboard.security.ldap.LDAPUserDetailsContextMapper;
-
 // Place your Spring DSL code here
 beans = {
-	
-	// For LDAP
-	ldapUserDetailsMapper(LDAPUserDetailsContextMapper) {
-		grailsApplication = ref("grailsApplication")
-	}
-	
-	applicationListener(LDAPAuthenticationApplicationListener)
 	
 }
