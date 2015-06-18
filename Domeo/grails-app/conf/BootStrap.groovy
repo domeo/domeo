b13ccc06d4fa599505806fafdf26d7e959097d4a
Paolo Ciccarese <paolo.ciccarese@gmail.com>
2015-06-17 22:30:18 -0400

    Removed unused code

--- Domeo/grails-app/conf/BootStrap.groovy
+++ Domeo/grails-app/conf/BootStrap.groovy
@@ -26,24 +26,7 @@ class BootStrap {
 	def springSecurityService
 
     def init = { servletContext ->
-		
-		/*
-		String password = 'password'
-		
-		def roleAdmin = new Role(authority: 'ROLE_ADMIN').save()
-		def roleUser = new Role(authority: 'ROLE_USER').save()
 
-		def user = new User(username: 'user',
-			password: password, enabled: true).save()
-		def admin = new User(username: 'admin',
-			password: password, enabled: true).save()
-
-		UserRole.create user, roleUser
-		UserRole.create admin, roleUser
-		UserRole.create admin, roleAdmin, true
-		
-		//org.hsqldb.util.DatabaseManager.main()
-*/
 		log.info  '========================================================================';
 		log.info  ' DOMEO ANNOTATION WEB TOOLKIT (v.' +
 			grailsApplication.metadata['app.version'] + ", b." +
